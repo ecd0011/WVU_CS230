20f14bbe9fa4e497dfa806e5fac1ddadc4272914
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-03 16:41:59 -0400

    Top hero, assets, and updated main app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopHeroComponent } from './body/top-hero/top-hero.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { SearchComponent } from './header/searchbar/search.component';
+import { BodyCardComponent } from './body/body-card/body-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    TopHeroComponent,
+    SearchComponent,
+    BodyCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/body/top-hero/top-hero.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-top-hero',
+    templateUrl: 'top-hero.component.html'
+})
+export class TopHeroComponent{
+
+}
