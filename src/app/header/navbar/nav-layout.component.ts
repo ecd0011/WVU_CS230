52c4a0c1352ccaae101853d229909a3ca9168e5d
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-14 12:49:38 -0400

    nav css

--- src/app/header/navbar/nav-layout.component.ts
+++ src/app/header/navbar/nav-layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-nav-layout',
-    templateUrl: 'nav-layout.component.html'
+    templateUrl: 'nav-layout.component.html',
+    styleUrls: ['./nav.component.css']
 })
 export class NavLayoutComponent{
 
--- src/app/header/navbar/nav-right.component.ts
+++ src/app/header/navbar/nav-right.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-nav-right',
-    templateUrl: 'nav-right.component.html'
+    templateUrl: 'nav-right.component.html',
+    styleUrls: ['./nav.component.css']
 })
 export class NavRightComponent{
 
