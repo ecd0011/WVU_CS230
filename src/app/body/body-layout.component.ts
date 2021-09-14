78fe98bdf68707a88485a42f8179aa0a9890b337
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-14 12:47:45 -0400

    body-layout css

--- src/app/body/body-layout.component.ts
+++ src/app/body/body-layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-body-layout',
-    templateUrl: 'body-layout.component.html'
+    templateUrl: 'body-layout.component.html',
+    styleUrls: ['./body-layout.component.css']
 })
 export class BodyLayoutComponent{
     approval = "94% of Chegg customers say they get better grades when they use Chegg to understand their coursework"
