963a160f04d255634fac7315472e61456732a5b8
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-14 12:47:54 -0400

    button css

--- src/app/body/btn.component.ts
+++ src/app/body/btn.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-btn',
-    templateUrl: 'btn.component.html'
+    templateUrl: 'btn.component.html',
+    styleUrls: ['./btn.component.css']
 })
 export class BTNComponent{
     @Input()
