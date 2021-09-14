c7b389dbcbfea6267e86b1e7e0a1a8a1deef4c76
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-14 12:48:05 -0400

    body card css

--- src/app/body/body-card/body-card.component.ts
+++ src/app/body/body-card/body-card.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-body-card',
-    templateUrl: 'body-card.component.html'
+    templateUrl: 'body-card.component.html',
+    styleUrls: ['./body-card.component.css']
 })
 export class BodyCardComponent{
     btntxt = "Find your books"
