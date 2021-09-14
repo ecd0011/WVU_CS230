7f5544ece036250ed878ed08b0336a9d4508fc04
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-14 12:48:13 -0400

    tile layout css

--- src/app/body/multi-tile/tile-layout.component.ts
+++ src/app/body/multi-tile/tile-layout.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-tile-layout',
-    templateUrl: 'tile-layout.component.html'
+    templateUrl: 'tile-layout.component.html',
+    styleUrls: ['./tile-layout.component.css']
 })
 export class TileLayoutComponent{
     label = "Sign up"
