f8ff9071c2884657334c57e40ef0f314a4a59ca3
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-14 12:48:29 -0400

    top hero css

--- src/app/body/top-hero/top-hero.component.ts
+++ src/app/body/top-hero/top-hero.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'app-top-hero',
-    templateUrl: 'top-hero.component.html'
+    templateUrl: 'top-hero.component.html',
+    styleUrls: ['./top-hero.component.css']
 })
 export class TopHeroComponent{
     btntxt = "Get study support"
