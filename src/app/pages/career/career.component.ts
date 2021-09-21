7fa27cf0a64ea953dff0729c5c7dbb400c41efba
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-21 17:05:12 -0400

    Adding career page

--- src/app/pages/career/career.component.ts
+++ src/app/pages/career/career.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'chegg-career',
-  templateUrl: './career.component.html'
+  templateUrl: './career.component.html',
+  styleUrls: ['./career.component.css']
 })
 export class CareerComponent implements OnInit {
 
