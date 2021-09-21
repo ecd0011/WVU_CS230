437a4644c7d14399f88d7e8c79be9eba1fb05c6b
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-21 17:05:28 -0400

    Adding life page/components

---
+++ src/app/pages/life/life-hero.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-life-hero',
+    templateUrl: 'life-hero.component.html',
+    styleUrls: ['./life-hero.component.css']
+})
+export class LifeHeroComponent{
+    
+}
