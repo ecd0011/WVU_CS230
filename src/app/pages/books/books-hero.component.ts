5635d6c64d1222ac3a48d673f464829c7d158542
ecd0011 <ecd0011@mix.wvu.edu>
2021-09-21 17:04:58 -0400

    Adding books page/components

---
+++ src/app/pages/books/books-hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-books-hero',
+    templateUrl: 'books-hero.component.html',
+    styleUrls: ['./books-hero.component.css']
+})
+export class BooksHeroComponent{
+    class = "navbar navbar-light bg-transparent mb-3 pe-2"
+    newWidth = "width: 590px;"
+}
