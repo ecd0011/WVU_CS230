e15be223e7545932921e756c4482ed7f513fb0c7
ecd0011 <ecd0011@mix.wvu.edu>
2021-10-16 16:17:05 -0400

    using service to obtain user information

--- src/app/body/user-info.component.ts
+++ src/app/body/user-info.component.ts
@@ -1,8 +1,7 @@
-import { HttpClient } from "@angular/common/http";
 import { Component, Injectable, OnInit } from "@angular/core";
 import { UserInfo } from "./user-info.model";
+import { UserInfoService } from "./user-info.service";
 
-@Injectable()
 @Component({
     selector: 'chegg-user-info',
     templateUrl: 'user-info.component.html'
@@ -10,20 +9,16 @@ import { UserInfo } from "./user-info.model";
 export class UserInfoComponent implements OnInit{
     myInfo: UserInfo | undefined;
     
-    constructor(private http: HttpClient){
+    constructor(private userInfoService: UserInfoService){
     }
     ngOnInit(): void {
-        console.log("Sending a get request to the server");
-        this.getUserInfo();
         console.log("Registering showUserInfo as a subscriber");
         this.showUserInfo();
     }
 
-    getUserInfo(){
-        return this.http.get<UserInfo>('https://chegg-app-cf77c-default-rtdb.firebaseio.com/my-info.json');
-    }
+    
     showUserInfo(){
-        this.getUserInfo().subscribe((data: UserInfo) => {
+        this.userInfoService.getUserInfo().subscribe((data: UserInfo) => {
             console.log(data);
             this.myInfo = data;
         })
